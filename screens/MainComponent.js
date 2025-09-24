80f02c42eff598135b6d2873d44ff4597c1f4a19
Sosina <sosinakassa12@gmail.com>
2025-09-23 20:00:02 -0500

    Forms

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -16,6 +16,7 @@ import { fetchPartners } from '../features/partners/partnersSlice';
 import { fetchCampsites } from '../features/campsites/campsitesSlice';
 import { fetchPromotions } from '../features/promotions/promotionsSlice';
 import { fetchComments } from '../features/comments/commentsSlice';
+import ReservationScreen from './ReservationScreen';
 
 const screenOptions = {
     headerTintColor: '#fff',
@@ -129,6 +130,29 @@ const ContactNavigator = () => {
     );
 };
 
+const ReservationNavigator = () => {
+    const Stack = createStackNavigator();
+    return (
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen 
+            name="Reservation" 
+            component={ReservationScreen} 
+            options={({ navigation }) => ({
+            title: 'Reservation Search',
+            headerLeft: () => (
+                <Icon
+                    name='tree'
+                    type='font-awesome'
+                    iconStyle={styles.stackIcon}
+                    onPress={() => navigation.toggleDrawer()}
+                />
+            )
+            })} 
+            />
+        </Stack.Navigator>
+    );
+};
+
 const CustomDrawerContent = (props) => (
     <DrawerContentScrollView {...props}>
         <View style={styles.drawerHeader}>
@@ -197,6 +221,22 @@ const Main = () => {
                             />
                         )
                     }}
+                    />
+                    <Drawer.Screen
+                        name='ReserveCampsite'
+                        component={ReservationNavigator}
+                        options={{
+                            title: 'Reserve Campsite',
+                            drawerIcon: ({ color }) => (
+                                <Icon
+                                    name='tree'
+                                    type='font-awesome'
+                                    size={24}
+                                    iconStyle={{ width: 24 }}
+                                    color={color}
+                                />
+                            )
+                        }}
                     />
                      <Drawer.Screen
                     name="AboutNav"
